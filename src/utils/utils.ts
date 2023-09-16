2201be682091e3b2d8618cee2907324939e0a4c6
Stephen Hanjaya <stephenhanjaya@gmail.com>
2023-09-15 20:56:46 -0700

    add method to read-send html/css

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -19,6 +19,37 @@ export function writeDataToFile(filePath: string, content: object) {
   });
 }
 
+export function sendHTML(
+  filePath: string,
+  res: http.ServerResponse<http.IncomingMessage>
+) {
+  fs.readFile(filePath, (err, readResponse) => {
+    if (err) {
+      res.writeHead(404, { "Content-Type": "application/json" });
+      res.write(JSON.stringify({ message: "Template not found." }));
+    } else {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(readResponse);
+    }
+    res.end();
+  });
+}
+
+export function sendCSS(
+  filePath: string,
+  res: http.ServerResponse<http.IncomingMessage>
+) {
+  fs.readFile(filePath, (err, readResponse) => {
+    if (err) {
+      console.error(err);
+    } else {
+      res.writeHead(200, { "Content-Type": "text/css" });
+      res.write(readResponse);
+    }
+    res.end();
+  });
+}
+
 // method to extract values from POST request body
 export function extractPOSTData(req: http.IncomingMessage) {
   return new Promise((resolve, reject) => {

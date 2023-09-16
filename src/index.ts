99a3e0516fe0808ab9054ddf2190164a4fd9f16c
Stephen Hanjaya <stephenhanjaya@gmail.com>
2023-09-15 20:58:46 -0700

    update index.ts to response req for html/css

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import http from "http";
 
 import { ProductController } from "./controllers/product.controller";
+import { sendHTML, sendCSS } from "./utils/utils";
 
 const hostname = "127.0.0.1";
 const port = 3000;
@@ -9,6 +10,25 @@ const productController = new ProductController();
 
 const server = http.createServer((req, res) => {
   const { url, method } = req;
+
+  // when html sent to client, html imports will cause subsequent calls for the imports
+  if (url?.endsWith(".css") && method === "GET") {
+    const urlArray = url.split("/");
+    const fileName = urlArray[urlArray.length - 1];
+    const fileFolder = fileName.slice(0, fileName.indexOf(".css"));
+
+    return sendCSS(`src/views/${fileFolder}/${fileName}`, res);
+  }
+
+  if (url === "/form/product") {
+    switch (method) {
+      case "GET":
+        return sendHTML(
+          "src/views/product.component/product.component.html",
+          res
+        );
+    }
+  }
 
   if (url === "/api/products") {
     switch (method) {

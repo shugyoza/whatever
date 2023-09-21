9ea4a983a7f808525e401238f5fd09d52e126e13
Stephen Hanjaya <stephenhanjaya@gmail.com>
2023-09-20 20:02:18 -0700

    rename db/=>db.local/,adjust.I want to have other db

--- src/models/db.local/product.model.ts
+++ src/models/db.local/product.model.ts
@@ -1,5 +1,5 @@
-import { Product } from "./db/products.interface";
-import { writeDataToFile } from "./utils/utils";
+import { Product } from "./products.interface";
+import { writeDataToFile } from "../utils/utils";
 
 export class ProductModel {
   public filePath: string;
